8dfe30b2577acd373f69ff146c24e7b67daa891e
FDC_Tongji <2450614@tongji.edu.cn>
2026-03-20 19:03:50 +0800

    restricted

--- Loongson_2K300_301_LIB/example/src/PID_control_test.cpp
+++ Loongson_2K300_301_LIB/example/src/PID_control_test.cpp
@@ -10,7 +10,7 @@
 int calculate_diffrential(float error)//给我误差值，给你差分输入值
         {        
         float Diffrential=0;//diffrencial 差分输入，即输出轮胎的转速差
-       float P = 1; // 比例系数
+       float P = 10; // 比例系数
        float D= 0.1; // 差分系数
        volatile static float error_current,error_last;// 当前误差和上一次误差
        error_current=error;//当前误差
@@ -31,8 +31,10 @@ void PID_control_test(void)
     // 拷贝赋值使用方法, 调用 pwm4 与调用 pwm2 同效
     ls_atim_pwm pwm4 = pwm2;*/
      int diffrential = calculate_diffrential(0);
-      ls_atim_pwm pwm1(ATIM_PWM0_PIN81, 100, 1000+diffrential);
-    ls_atim_pwm pwm2(ATIM_PWM1_PIN82, 100, 1000-diffrential);//从1000开始，差分输入增加pwm1的占空比，减少pwm2的占空比
+     int pwm1_duty1 = (1000 + diffrential < 5000) ? (1000 + diffrential) : 5000;//从1000开始，差分输入增加pwm1的占空比,并且做了限幅（不得大于5000）
+      int pwm2_duty1 = (1000 - diffrential > 0) ? (1000 - diffrential) : 0;//从1000开始，差分输入减少pwm2的占空比，并且做了限幅（不得小于0）
+    ls_atim_pwm pwm1(ATIM_PWM0_PIN81, 100, pwm1_duty1);
+    ls_atim_pwm pwm2(ATIM_PWM1_PIN82, 100, pwm2_duty1);
 
 pwm1.atim_pwm_set_duty(0);
 pwm2.atim_pwm_set_duty(0);//全部失能
