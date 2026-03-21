95866da38ef3465acc6b08dc07f95fa0d6805071
FDC_Tongji <2450614@tongji.edu.cn>
2026-03-21 15:15:31 +0800

    control_demo

--- Loongson_2K300_301_LIB/example/src/PID_control_demo.cpp
+++ Loongson_2K300_301_LIB/example/src/PID_control_demo.cpp
@@ -10,7 +10,7 @@
 int calculate_diffrential(float error)//给我误差值，给你差分输入值
         {        
         float Diffrential=0;//diffrencial 差分输入，即输出轮胎的转速差
-       float P = 10; // 比例系数
+       float P = 5; // 比例系数
        float D= 0.1; // 差分系数
        volatile static float error_current,error_last;// 当前误差和上一次误差
        error_current=error;//当前误差
@@ -26,8 +26,8 @@ void PID_control_test(float error)
     ls_atim_pwm pwm2(ATIM_PWM1_PIN82, 100, 0);//初始化PWM，初始占空比为0
    
      int diffrential = calculate_diffrential(error);
-     int pwm1_duty1 = (1000 + diffrential < 5000) ? (1000 + diffrential) : 5000;//从1000开始，差分输入增加pwm1的占空比,并且做了限幅（不得大于5000）
-      int pwm2_duty1 = (1000 - diffrential > 0) ? (1000 - diffrential) : 0;//从1000开始，差分输入减少pwm2的占空比，并且做了限幅（不得小于0）
+     int pwm1_duty1 = (500 + diffrential < 1000) ? (500 + diffrential) : 5000;//从500开始，差分输入增加pwm1的占空比,并且做了限幅（不得大于5000）
+      int pwm2_duty1 = (500 - diffrential > 0) ? (500 - diffrential) : 0;//从500开始，差分输入减少pwm2的占空比，并且做了限幅（不得小于0）
     ls_atim_pwm pwm1(ATIM_PWM0_PIN81, 100, pwm1_duty1);
     ls_atim_pwm pwm2(ATIM_PWM1_PIN82, 100, pwm2_duty1);
 
