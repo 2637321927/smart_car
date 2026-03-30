d3233fc83168573c7785d7ff5c12bb00cd031b1e
孙琮贺 <16274942+sun-conghe1@user.noreply.gitee.com>
2026-03-30 16:02:38 +0800

    sd

--- Loongson_2K300_301_LIB/example/src/img_test.cpp
+++ Loongson_2K300_301_LIB/example/src/img_test.cpp
@@ -7,8 +7,8 @@ typedef signed short       sint16;  // 16位 有符号  <-- 你要的
 // =====================================================
 // 目标IP地址（UDP接收端）
 uint8_t otsuThreshold(uint8_t *image, uint16_t col, uint16_t row);
-ls_atim_pwm pwm1(ATIM_PWM0_PIN81, 100, 0);
-ls_atim_pwm pwm2(ATIM_PWM1_PIN82, 100, 0); 
+ls_atim_pwm pwm1(ATIM_PWM0_PIN81, 100, 9000);
+ls_atim_pwm pwm2(ATIM_PWM1_PIN82, 100, 9000); 
 typedef signed short sint16;
 
 
@@ -41,7 +41,7 @@ sint16 Search_Stop_Line;
 // 每列白色像素统计
 // ====================
 sint16 White_Column[LCDW]; 
-const std::string TARGET_IP    = "192.168.43.146";
+const std::string TARGET_IP    = "192.168.43.213";
 //192.168.43.146 huawei
 //192.168.43.213 lianxiang
 // UDP目标端口
@@ -706,44 +706,6 @@ void img_test(void)
     auto start_time = std::chrono::high_resolution_clock::now();
 
     printf("Start streaming... Press Ctrl+C to stop\r\n");
-
-
-//轮胎pd调速测试：输入你想的转速
-
-// 轮胎PD调速：输入目标转速（纯输入板块）
-int expected_speed_of_motor1_rps = 0;
-int expected_speed_of_motor2_rps = 0;
-const int MAX_SPEED = 300;
-const int MIN_SPEED = 0;
-    pwm1.atim_pwm_set_duty(0);
-    pwm2.atim_pwm_set_duty(0);
-printf("请输入电机1、电机2目标转速(rps，空格分隔)：");
-// 读取两个int型数据
-int res = scanf("%d %d", &expected_speed_of_motor1_rps, &expected_speed_of_motor2_rps);
-
-// 合法性判断
-if (res == 2) 
-{
-    if (expected_speed_of_motor1_rps >= MIN_SPEED && expected_speed_of_motor1_rps <= MAX_SPEED &&
-        expected_speed_of_motor2_rps >= MIN_SPEED && expected_speed_of_motor2_rps <= MAX_SPEED)
-    {
-        printf("输入正确！电机1：%d，电机2：%d\n", expected_speed_of_motor1_rps, expected_speed_of_motor2_rps);
-        // 这里可以直接调用你的闭环控制函数
-    }
-    else
-    {
-        printf("输入错误：转速超出范围！\n");
-    }
-}
-else
-{
-    printf("输入错误：请输入两个整数！\n");
-    fflush(stdin); // 清空输入缓存
-}
-
-
-
-
     while (true) {
         if (has_input()) {
             char c = getchar();
@@ -806,19 +768,8 @@ cv::rectangle(crop_img, cv::Point(x1, y1), cv::Point(x1 + 20, y1 + 20), cv::Scal
         compressimage(gray_frame);  // 压缩
         Ostu();      
         Longest_White_Column();
-       // std::cout<<Mid_Line[40]- 40<<std::endl; 
+        std::cout<<Mid_Line[40]- 40<<std::endl; 
      //  PID_control_test(pwm1,pwm2,Mid_Line[40]- 40);
-
-//以下开始测试轮胎闭环控制
-float speed_of_motor1=enc1.encoder_get_count();
-float speed_of_motor2=enc2.encoder_get_count();
-close_circle_control(
-    pwm1,pwm2,
-   speed_of_motor1,speed_of_motor2,
-    expected_speed_of_motor1_rps,
-    expected_speed_of_motor2_rps
-    );
-    
 
 
 
