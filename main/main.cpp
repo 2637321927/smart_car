98e449da823b59588d9ad0420b94b45f5bfd079d
FDCtongji <2450614@tongji.edu.cn>
2026-04-09 23:50:03 +0800

    new frame bug emerges

--- main/main.cpp
+++ main/main.cpp
@@ -16,6 +16,12 @@ ls_encoder_pwm enc1(ENC_PWM1_PIN65, PIN_73);
 int set_speed_of_motor1_rps=0;
 int set_speed_of_motor2_rps=0;
 lq_udp_client udp_client;
+// 摄像头参数
+const uint16_t    CAM_WIDTH    = 160;     // 宽
+const uint16_t    CAM_HEIGHT   = 120;     // 高
+const uint16_t    CAM_FPS      = 120;     // 帧率
+static struct termios old_tio;
+    lq_camera cam(CAM_WIDTH, CAM_HEIGHT, CAM_FPS);
 int mid;
 void handle_exit(int sig)
 {
@@ -36,23 +42,57 @@ void handle_exit(int sig)
     // 标记需要退出
     need_exit = true;
 }
+// 开启 非阻塞输入
+void set_terminal_nonblock() {
+    struct termios new_tio;
+    tcgetattr(STDIN_FILENO, &old_tio);
+    new_tio = old_tio;
+
+    // 关闭 行缓冲 + 关闭回显
+    new_tio.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+// 恢复终端（非常重要！）
+void reset_terminal() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
+}
+
+// 判断：有没有按键输入？
+bool has_input() {
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+
+    // 超时 0 → 不等待，直接返回
+    struct timeval tv = {0, 0};
+    select(STDIN_FILENO + 1, &fds, nullptr, nullptr, &tv);
+
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
 // 全局变量，保存原来的终端模式
 int main()
 {
 
 
 input_speed_rps(set_speed_of_motor1_rps,set_speed_of_motor2_rps);
+start_camera();
+set_terminal_nonblock();
+
 
    speed_timer.set_seconds_ms(5, []() {
-       test_enc_and_motor_rps;        // 直接调用你封装好的速度函数
+       test_enc_and_motor_rps;      
+       std::cout<<"fuck you"<<std::endl;  // 直接调用你封装好的速度函数
     });
 
     dir_timer.set_seconds_ms(10, []() {
         PID_control_test(latest_error);   // 直接调用你封装好的方向函数
     });
-start_camera();
+std::cout<<"fuck you2"<<std::endl; 
+
 while (1)
 {
+    /*
          if (has_input()) {
             char c = getchar();
             if (c == 'q') {
@@ -61,7 +101,9 @@ while (1)
                  while (getchar() != EOF); 
                 break;
             } 
+            std::cout<<"fuck you3"<<std::endl; 
         }
+            */
  img_test();
 
 }
