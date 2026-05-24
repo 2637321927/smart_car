920fac5e3600ae883a29b0ac2b0973830ec7ff24
孙琮贺 <16274942+sun-conghe1@user.noreply.gitee.com>
2026-05-24 13:30:02 +0800

    camera

--- main/build/CMakeFiles/main.dir/home/suncongsun/Desktop/smart_car/Loongson_2K300_301_LIB/libraries/drv/src/lq_camera_ex.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main.dir/home/suncongsun/Desktop/smart_car/Loongson_2K300_301_LIB/libraries/drv/src/lq_camera_ex.cpp.o: \
- /home/suncongsun/Desktop/smart_car/Loongson_2K300_301_LIB/libraries/drv/src/lq_camera_ex.cpp \
- /home/suncongsun/Desktop/smart_car/Loongson_2K300_301_LIB/tools/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.6/loongarch64-linux-gnu/sysroot/usr/include/stdc-predef.h
